8ea3389ebc7e06ef876fc69236f2b65a87a46905
killua173 <hamzaab25@gmail.com>
2025-03-19 16:41:05 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-    basePath: "/lawyer-website", // Replace with your GitHub repo name
-    trailingSlash: true,
-  };
-  
-  module.exports = nextConfig;
-  
+ /** @type {import('next').NextConfig} */
+ const nextConfig = {
+    output: "exports", // lightweight but no dynamic routes & api support
+    // output: "standalone", // bulkier but supports dynamic routes & api
+}
+
+module.exports = nextConfig
