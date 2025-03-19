62aeced10d2ecae0ed8642f2e0e87f889c2342b6
killua173 <hamzaab25@gmail.com>
2025-03-19 16:27:20 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: "/your-repo-name", // Replace with your GitHub repo name
+    basePath: "/lawyer-website", // Replace with your GitHub repo name
     trailingSlash: true,
   };
   
